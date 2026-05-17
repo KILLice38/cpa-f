fa41b0c94eaeb1c2797accf0118d917e99d34168
KILLice38 <kirillos382000@mail.ru>
2026-05-17 19:29:38 +0300

    feat(i18n): static rendering for localized routes enabled

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,3 +1,6 @@
-export default function Home() {
+import { setRequestLocale } from 'next-intl/server';
+export default async function Home({ params }: { params: Promise<{ locale: string }> }) {
+	const { locale } = await params;
+	setRequestLocale(locale);
 	return <main></main>;
 }
