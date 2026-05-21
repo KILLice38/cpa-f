778025247ffb9708481e66c0b77b68c77c5f1b0b
Yuri Kuznetsov <playeer2905@gmail.com>
2026-05-21 14:18:51 +0300

    style: fix font paths and add gradient variable

--- src/shared/fonts/index.ts
+++ src/shared/fonts/index.ts
@@ -3,23 +3,23 @@ import localFont from 'next/font/local';
 export const fontHalvarBreit = localFont({
 	src: [
 		{
-			path: '../shared/assets/fonts/HalvarBreit-Lt.ttf',
+			path: '../assets/fonts/HalvarBreit-Lt.ttf',
 			weight: '300',
 			style: 'normal',
 		},
 		{
-			path: '../shared/assets/fonts/HalvarBreit-Rg.ttf',
+			path: '../assets/fonts/HalvarBreit-Rg.ttf',
 			weight: '400',
 			style: 'normal',
 		},
 		{
-			path: '../shared/assets/fonts/HalvarBreit-Md.ttf',
+			path: '../assets/fonts/HalvarBreit-Md.ttf',
 			weight: '500',
 			style: 'normal',
 		},
 		{
-			path: '../shared/assets/fonts/HalvarBreit-Bd.ttf',
-			weight: '700',
+			path: '../assets/fonts/HalvarBreit-Bd.ttf',
+			weight: '800',
 			style: 'normal',
 		},
 	],
@@ -29,12 +29,12 @@ export const fontHalvarBreit = localFont({
 export const fontStolzl = localFont({
 	src: [
 		{
-			path: '../shared/assets/fonts/Stolzl-Light.ttf',
+			path: '../assets/fonts/Stolzl-Light.ttf',
 			weight: '300',
 			style: 'normal',
 		},
 		{
-			path: '../shared/assets/fonts/Stolzl-Regular.ttf',
+			path: '../assets/fonts/Stolzl-Regular.ttf',
 			weight: '400',
 			style: 'normal',
 		},
