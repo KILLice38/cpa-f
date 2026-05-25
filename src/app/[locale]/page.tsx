2757eb27c6c063758bca9d2fb3414a4c57351a7a
KILLice38 <kirillos382000@mail.ru>
2026-05-25 16:43:22 +0300

    feat: homeClient import in page added

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,6 +1,12 @@
 import { setRequestLocale } from 'next-intl/server';
+
+import { HomeClient } from './HomeClient';
 export default async function Home({ params }: { params: Promise<{ locale: string }> }) {
 	const { locale } = await params;
 	setRequestLocale(locale);
-	return <main></main>;
+	return (
+		<main>
+			<HomeClient />
+		</main>
+	);
 }
