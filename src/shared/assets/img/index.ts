97dbc84a987c1e205df0ef9bfd6cc71357cf6544
Yuri Kuznetsov <yurikuznetsov2905@gmail.com>
2026-05-30 23:05:31 +0300

    feat(ui): add TasksGrid component

---
+++ src/shared/assets/img/index.ts
@@ -0,0 +1,3 @@
+export { default as heroImg } from './hero-footer.png';
+export { default as mainTasksDesktopImg } from './main-tasks-desktop.png';
+export { default as mainTasksMobileImg } from './main-tasks-mobile.png';
---
+++ src/shared/assets/index.ts
@@ -0,0 +1,1 @@
+export { heroImg, mainTasksDesktopImg, mainTasksMobileImg } from './img';
