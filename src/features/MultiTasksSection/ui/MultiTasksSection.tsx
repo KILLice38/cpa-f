756823dea6ebeed0e07882c618537631d1fc1574
Yuri Kuznetsov <playeer2905@gmail.com>
2026-06-02 01:19:53 +0300

    feat(ui): adapt sections for mobile

--- src/features/MultiTasksSection/ui/MultiTasksSection.tsx
+++ src/features/MultiTasksSection/ui/MultiTasksSection.tsx
@@ -1,7 +1,7 @@
 import { getTranslations } from 'next-intl/server';
 
 import { getTasks } from '@/shared/api';
-import { Container, Section, SectionTitle } from '@/shared/ui';
+import { Container, MobileMenuButton, Section, SectionTitle } from '@/shared/ui';
 
 import styles from './MultiTasksSection.module.css';
 import { TasksGrid } from './TasksGrid';
